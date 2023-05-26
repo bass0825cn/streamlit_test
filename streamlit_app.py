ad29989da044c5cf46471645b8d98135a261cd23
Song DaCai <bass0825cn@sina.com>
2023-05-26 10:38:29 +0800

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,31 @@
+# -*- coding: UTF-8 -*-
+
+import streamlit as st
+import altair as alt
+import pandas as pd
+import numpy as np
+
+st.header('st.write')
+
+st.write('Hello *World!* :sunglasses:')
+
+st.write(1234)
+
+df = pd.DataFrame({
+     'first column': [1, 2, 3, 4],
+     'second column': [10, 20, 30, 40]
+     })
+st.write(df)
+
+# 样例 4
+
+st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
+
+# 样例 5
+
+df2 = pd.DataFrame(
+     np.random.randn(200, 3),
+     columns=['a', 'b', 'c'])
+c = alt.Chart(df2).mark_circle().encode(
+     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+st.write(c)
